6bbbea0ecbef378310fd20736bcc5ae4d26dffdb
Roman Ochsenreiter <romanoch@tbi.univie.ac.at>
2016-10-28 17:51:41 +0200

    Implemented Draft of the CompoundCmHit class with error handling

--- CmAnalysis/CmDataStrucs.py
+++ CmAnalysis/CmDataStrucs.py
@@ -4,7 +4,10 @@ class CmHit:
     '''CmHit - Store one result of cmsearch in easy and accessible manner
     '''
 
-    def __init__(self, dic):
+    def __init__(self, dic=None):
+        if dic is None:
+            raise ValueError('No dictionary given')
+
         self.seqname = dic['name']
         self.seq_accession = dic['accession']
         self.cm = dic['query_name']
@@ -28,7 +31,7 @@ class CmHit:
         self.inc = dic['inc']
 
         self.struc = None
-        self.seq = None
+        self.seq = None #Just gets filled when parsing from stdout
 
         # Omitted, serve no practical purpose
         # self.pass = dic['pass']
@@ -53,6 +56,50 @@ class CmHit:
             raise ValueError('No Sequence found')
 
 class CompoundCmHit:
-    '''CompoundCmHit - Store one result of cmsearch in easy and accessible manner
+    '''CompoundCmHit - Store overlapping hits of cmsearch
+    Args:
+        seed_hit(CmHit object)
+    Raises:
+        ValueError - No Seed was given for initialization
+        TypeError - Seed was not CmHit object
     '''
-    pass
+    from utils import Overlap
+
+    def __init__(self, seed_hit=None):
+        if seed_hit is None:
+            raise ValueError('Needs Seed for initialization')
+        elif not isinstance(seed_hit, CmHit):
+            raise TypeError('Wrong Seed Hit format (must be CmHit)')
+        self.overlap_threshold = 0.90
+        self.seqname  = seed_hit.seqname
+        self.seqID    = seed_hit.accession
+        self.seq_from = seed_hit.seq_from
+        self.seq_to   = seed_hit.seq_to
+        self.range =  seed_hit.range
+        self.cms   =  []
+        self.cms.append((seed_hit.cm, seed_hit.evalue, seed_hit.seq_range))
+
+    def add_hit(self, hit):
+        '''Add a hit to an existing CompountCmHit object
+        Args:
+            hit(CmHit)
+        Returns:
+            True - Hit added successfully
+            False - Hit is not overlapping with seed hit and was not added
+        Raises:
+            TypeError - Hit is not CmHit object
+            ValueError - Hit is on different sequence than seed hit
+        '''
+        if not isinstance(hit, CmHit):
+            raise TypeError('Wrong Seed Hit format (must be CmHit)')
+        elif not hit.seqname == self.seqname
+            raise ValueError('Hit belongs to different seq, seed: ' + self.seqname)
+        elif Overlap(self.seq_from, self.seq_to, hit.seq_from, hit.seq_to) > self.overlap_threshold:
+            self.cms.append((hit.cm, hit.evalue, hit.seq_range))
+            return True
+        else:
+            return False
+
+
+    def test(self):
+        pass
